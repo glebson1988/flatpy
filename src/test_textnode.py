6f8856cc0b08a31325aac950c10e0726832d9e86
glebson1988 <glebson1988@yandex.ru>
2025-06-03 19:58:15 +0200

    First tests and starting htmlnode

---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_text(self):
+        node = TextNode("Text A", TextType.BOLD)
+        node2 = TextNode("Text B", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_text_type(self):
+        node = TextNode("Text", TextType.TEXT)
+        node2 = TextNode("Text", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url(self):
+        node = TextNode("Text", TextType.LINK, "http://example.com")
+        node2 = TextNode("Text", TextType.LINK, "http://other.com")
+        self.assertNotEqual(node, node2)
+
+    def test_eq_with_none_url(self):
+        node = TextNode("Text", TextType.LINK, url=None)
+        node2 = TextNode("Text", TextType.LINK)
+        self.assertEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
