f3300736e29d99bf90033304705461717db8ebc0
glebson1988 <glebson1988@yandex.ru>
2025-06-05 19:51:29 +0200

    Converter with tests

---
+++ src/converter.py
@@ -0,0 +1,25 @@
+from htmlnode import LeafNode
+from textnode import TextNode, TextType
+
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node, TextNode):
+        raise ValueError("Input must be a TextNode")
+    
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(tag=None, value=text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        if text_node.url is None:
+            raise ValueError("Link TextNode must have a URL")
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        if text_node.url is None:
+            raise ValueError("Image TextNode must have a URL")
+        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+    else:
+        raise ValueError(f"Unsupported TextType: {text_node.text_type}")
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,8 @@
 import unittest
 
 from htmlnode import HTMLNode, LeafNode, ParentNode
+from textnode import TextNode, TextType
+from converter import text_node_to_html_node
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_single_attribute(self):
@@ -50,3 +52,73 @@ class TestHTMLNode(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             ParentNode("div", [])
         self.assertEqual(str(context.exception), "All parent nodes must have children")
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.props, {})
+
+    def test_bold(self):
+        node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold text")
+        self.assertEqual(html_node.props, {})
+
+    def test_italic(self):
+        node = TextNode("Italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italic text")
+        self.assertEqual(html_node.props, {})
+
+    def test_code(self):
+        node = TextNode("Code text", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "Code text")
+        self.assertEqual(html_node.props, {})
+
+    def test_link(self):
+        node = TextNode("Click me", TextType.LINK, url="https://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me")
+        self.assertEqual(html_node.props, {"href": "https://example.com"})
+
+    def test_image(self):
+        node = TextNode("Alt text", TextType.IMAGE, url="https://example.com/image.jpg")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "https://example.com/image.jpg", "alt": "Alt text"})
+
+    def test_invalid_text_type(self):
+        class InvalidTextType:
+            value = "Invalid"
+        node = TextNode("Invalid text", InvalidTextType())
+        with self.assertRaises(ValueError) as context:
+            text_node_to_html_node(node)
+        self.assertTrue(str(context.exception).startswith("Unsupported TextType:"))
+
+    def test_link_no_url_raises_error(self):
+        node = TextNode("Click me", TextType.LINK)
+        with self.assertRaises(ValueError) as context:
+            text_node_to_html_node(node)
+        self.assertEqual(str(context.exception), "Link TextNode must have a URL")
+
+    def test_image_no_url_raises_error(self):
+        node = TextNode("Alt text", TextType.IMAGE)
+        with self.assertRaises(ValueError) as context:
+            text_node_to_html_node(node)
+        self.assertEqual(str(context.exception), "Image TextNode must have a URL")
+
+    def test_non_text_node_raises_error(self):
+        invalid_node = "Not a TextNode"
+        with self.assertRaises(ValueError) as context:
+            text_node_to_html_node(invalid_node)
+        self.assertEqual(str(context.exception), "Input must be a TextNode")
